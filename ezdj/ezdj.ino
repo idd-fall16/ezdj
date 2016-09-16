b25cbb9e05ed09180473c3d9af9f6060f2e18667
Jingyi Li <noon@berkeley.edu>
2016-09-15 23:15:53 -0700

    init commit

---
+++ ezdj/ezdj.ino
@@ -0,0 +1,37 @@
+/******************
+ * EZDJ Duo code. Reads in voltages from analog sensors, prints to serial.
+ */
+
+// turn off cloud functionality
+#if defined(ARDUINO) 
+SYSTEM_MODE(MANUAL); 
+#endif
+
+const int pos = A1;
+const int amp = A2;
+const int tamber = A3;
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(pos, INPUT);
+  pinMode(amp, INPUT);
+  pinMode(tamber, INPUT);
+
+}
+
+void loop() {
+
+  pos_val = analogRead(pos);
+  Serial.printf("Position, %d\n", pos_val);
+
+  amp_val = analogRead(amp);
+  Serial.printf("Amplitude, %d\n", amp_val);
+    
+  tamber_val = analogRead(tamber);
+  Serial.printf("Tamber, %d\n", tamber_val); 
+  //note: what are the correct ranges?
+  
+  delay(1000);
+    
+  
+}
