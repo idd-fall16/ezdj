9d5588769bb6d92275dfa89b4ea1709cd453179a
Jingyi Li <noon@berkeley.edu>
2016-09-17 15:47:04 -0700

    handle another amp case

--- ezdj/ezdj.ino
+++ ezdj/ezdj.ino
@@ -10,10 +10,10 @@ SYSTEM_MODE(MANUAL);
 const int pos = A1;
 const int amp = A2;
 const int timbre = A3;
-const int mode = D1;
+const int mode = D3;
 bool pitch_mode = false;
 int old_amp = 0;
-const int amp_low = 3300; //low end of 'not pressed'
+const int amp_low = 3200; //low end of 'not pressed'
 const int amp_high = 3350; //high end of 'not pressed'
 
 void setup() {
@@ -25,27 +25,32 @@ void setup() {
 }
 
 void loop() {
-
   int mode_val = digitalRead(mode); 
   Serial.printf("/switch %d\n", mode_val);
-  
   int pos_val = analogRead(pos);
   Serial.printf("/position %d\n", pos_val);
     
   int amp_val = analogRead(amp);
-  if (amp_val > amp_low && amp_val < amp_high) {
+  if (amp_val > amp_low && amp_val <  amp_high ) {
     //user probably let go, remember old val instead
     amp_val = old_amp;
-  } else { //update the 'old val' w the correct one
+  } 
+  if (old_amp < 200) { //another case when it tries to interpolate
+      if (amp_val < 1800 && amp_val > 2900) { 
+        amp_val = old_amp; 
+    } else { 
+      old_amp = amp_val;
+      }
+  }
+  else { //update the 'old val' w the correct one
     old_amp = amp_val;
   }
   Serial.printf("/amplitude %d\n", amp_val);
   
   int timbre_val = analogRead(timbre);
   Serial.printf("/timbre %d\n", timbre_val); 
-  //note: what are the correct ranges?
   Serial.printf("Done \n");
-  delay(1500);
+  delay(50);
     
   
 }
